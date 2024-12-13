325a5f974859e4344184b1854ca0f9e87f497b24
jakeergit111 <jakeershaik66@gmail.com>
2024-12-13 17:36:36 +0530

    FirstProject

---
+++ firstproject/src/com/dhl/oops/Firstprogram.java
@@ -0,0 +1,10 @@
+package com.dhl.oops;
+
+public class Firstprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to Psoft");
+	}
+
+}
